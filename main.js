afb7be445f542173a47bebbf4fa326cb8ccc3885
steelsnake93 <nikolanikotasev@protonmail.com>
2022-11-15 20:02:40 +0100

    finished with styling, function for validation complete, to do media style yet.

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-(function() {
-    var form = document.getElementById ('password');
-    addEvent(form, 'submit', function(e) {
-        e.preventDefault();
-        var elements = this.elements;
-        var userName = elements.firstName.value;
-        var msg = 'Welcome' + userName;
-        document.getElementById('main').textContent = msg;
-    });
-}());
+document.addEventListener("DOMContentLoaded", function(){
 
+    let password = document.querySelector(".password").value;
+    let confirmPassword = document.querySelector(".confirm-password").value;
 
+    let error = document.querySelector("#error");
 
+    if(password == "" && confirmPassword == "") {
+        error.innerHTML = "*passwords do not mach";
+    } else if(password === confirmPassword) {
+        error.innerHTML = "";
+    }
+
+})
